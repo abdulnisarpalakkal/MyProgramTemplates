1be82edfa624ecc57a1a3157ba79484902de18ef
Nisar <35187185+abdulnisarpalakkal@users.noreply.github.com>
2019-01-07 10:17:58 +0400

    Update controller-module.ts

--- angular/controller-module.ts
+++ angular/controller-module.ts
@@ -1,1 +1,24 @@
 
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule   }   from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
+
+import { AdministrationRoutingModule } from './administration-routing.module';
+import { UserComponent } from './user/user.component';
+import { UserRolesComponent } from './user-roles/user-roles.component';
+import { AdministrationComponent } from './administration.component';
+import { PageHeaderModule } from '../../shared';
+import {UserModalComponent} from './user/modal/usermodal.component';
+import {NgxDatatableComponent} from './user/table/ngx-datatable-demo';
+import { UserRolesModalComponent } from './user-roles/user-roles-modal/user-roles-modal.component';
+
+
+
+@NgModule({
+    imports: [CommonModule, AdministrationRoutingModule, PageHeaderModule,FormsModule,NgxDatatableModule,NgbModule],
+    declarations: [UserComponent,UserModalComponent,NgxDatatableComponent,AdministrationComponent,UserRolesComponent,UserRolesModalComponent ]
+})
+export class AdministrationModule {}
