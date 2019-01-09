d83dfb670c96fbe161fc4fe4c447f066cbf905d9
Nisar <35187185+abdulnisarpalakkal@users.noreply.github.com>
2019-01-09 16:13:30 +0400

    Update dao.java

--- java/dao.java
+++ java/dao.java
@@ -2,19 +2,12 @@ package com.focowell.dao;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.focowell.model.ProcessData;
-
-
-
+import com.focowell.model.Category;
 
 @Repository
-public interface ProcessDao extends CrudRepository<ProcessData, Long> {
-	List<ProcessData> findByProcessName (String processName);
-	
-	@Query("SELECT p FROM ProcessData p left join fetch p.category ")
-	Iterable<ProcessData> findAllByJPQL();
+public interface CategoryDao extends CrudRepository<Category, Long> {
+	List<Category> findByCategoryName(String categoryName);
 }
